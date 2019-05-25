2173cdc0c77f4f3834622381c1da557407d75dd5
EvanHanna12 <45378721+EvanHanna12@users.noreply.github.com>
2019-05-25 15:57:44 -0400

    Create alert.js

---
+++ commands/alert.js
@@ -0,0 +1,35 @@
+var discord = require("discord.js")
+
+module.exports.run = (bot, message, args) => {
+  let msg = args.join(' ');
+  let em = new discord.RichEmbed()
+  .setTitle("BanUtils Alert")
+  .setDescription("Contacting MasterCoder...")
+  .setTimestamp()
+  .setColor("RED")
+
+  message.channel.send({embed: em})
+
+  let owner = bot.users.find(u => u.id == process.env.ownerID)
+
+  if (!msg) {
+    em
+    .setTitle("Alert!")
+    .setDescription(`${message.author.username} needs help in ${message.channel.name}!`)
+    .setFooter("No message provided.")
+    .setTimestamp()
+    .setColor("RANDOM")
+    owner.send({embed: em})
+  } else {
+    em
+    .setTitle("Alert!")
+    .setDescription(`${message.author.username} needs help in #${message.channel.name}!`)
+    .setTimestamp()
+    .setColor("RANDOM")   
+    owner.send({embed: em})
+  }
+}
+
+module.exports.help = {
+  name: "alert"
+}
